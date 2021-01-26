50e9eb38afc2df32b3458effa0d0a6ebfeeace9e
Simon <simon@simontam.ca>
2021-01-25 23:54:03 -0700

    Set up basic Python files and test env vars

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello world!")
+import os
+from dotenv import load_dotenv, dotenv_values
+
+def main():
+    load_dotenv()
+
+    env_vars = dotenv_values(".env")
+    print( env_vars )
+
+if __name__ == "__main__":
+    main()
